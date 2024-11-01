d0f6118330da46304c82d0bcca6f7b447fb597ea
BEQUANG2409 <21522507@gm.uit.edu.vn>
2024-11-01 12:26:36 +0700

    Create project Frontend JWT

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import Login from "./components/Login/Login";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app-container">
+        <Switch>
+          <Route path="/news">news</Route>
+          <Route path="/about">about</Route>
+          <Route path="/contact">contact</Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/" exact>
+            home
+          </Route>
+          <Route path="*">404 not Found</Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Login/Login.js
@@ -0,0 +1,10 @@
+const Login = () => {
+  return (
+    <div className="login-container">
+      Login
+      <button>hello</button>
+    </div>
+  );
+};
+
+export default Login;
