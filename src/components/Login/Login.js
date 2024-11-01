fceb2a60921dcccdc6a525ff58318c0f08b9f4de
BEQUANG2409 <21522507@gm.uit.edu.vn>
2024-11-01 16:33:07 +0700

    Dựng base cho trang Login và scss cho form đăng nhập

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,44 @@
+import "./Login.scss";
+
 const Login = () => {
   return (
     <div className="login-container">
-      Login
-      <button>hello</button>
+      <div className="container">
+        <div className="row">
+          <div className="content-left col-7">
+            <div className="brand">facebook</div>
+            <div className="detail">
+              Facebook helps you connect and share with the people in your life.
+            </div>
+          </div>
+          <div className="content-right col-5 d-flex justify-content-center bg-light">
+            <div className="d-flex flex-column gap-3 py-3 w-90">
+              <input
+                type="text"
+                className="form-control pt-3 pb-3"
+                placeholder="Email address or phone number"
+              />
+              <input
+                type="password"
+                className="form-control pt-3 pb-3"
+                placeholder="Password"
+              />
+              <button className="btn btn-primary pt-3 pb-3 fs-4 fw-bold">
+                Login
+              </button>
+              <div className="text-center color-blue">
+                <span>Forgotten password?</span>
+              </div>
+              <hr />
+              <div className="text-center">
+                <button className="btn btn-login text-white fw-bold fs-4">
+                  Create new account
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
